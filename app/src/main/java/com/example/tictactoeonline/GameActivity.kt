65f9109daadcb54cfc48244a5c1485b3c4550ba0
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2024-10-30 00:11:06 +0600

    game model

--- app/src/main/java/com/example/tictactoeonline/GameActivity.kt
+++ app/src/main/java/com/example/tictactoeonline/GameActivity.kt
@@ -1,11 +1,40 @@
 package com.example.tictactoeonline
 
 import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
 import androidx.appcompat.app.AppCompatActivity
+import com.example.tictactoeonline.databinding.ActivityGameBinding
 
-class GameActivity : AppCompatActivity() {
+class GameActivity : AppCompatActivity(),OnClickListener{
+
+    lateinit var binding: ActivityGameBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_game)
+        binding = ActivityGameBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.btn0.setOnClickListener(this)
+        binding.btn1.setOnClickListener(this)
+        binding.btn2.setOnClickListener(this)
+        binding.btn3.setOnClickListener(this)
+        binding.btn4.setOnClickListener(this)
+        binding.btn5.setOnClickListener(this)
+        binding.btn6.setOnClickListener(this)
+        binding.btn7.setOnClickListener(this)
+        binding.btn8.setOnClickListener(this)
+
+        binding.startGameBtn.setOnClickListener {
+            startGame();
+        }
+    }
+
+    fun startGame() {
+
+    }
+
+    override fun onClick(v: View?) {
+
     }
 }
