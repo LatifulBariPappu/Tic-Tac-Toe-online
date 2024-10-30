f5eec01f5afa66317099c934e0d73079c4fd0023
Latiful_Bari_Pappu <latifulbari2001@gmail.com>
2024-10-31 01:39:20 +0600

    ui functionality

--- app/src/main/java/com/example/tictactoeonline/GameActivity.kt
+++ app/src/main/java/com/example/tictactoeonline/GameActivity.kt
@@ -9,6 +9,9 @@ import com.example.tictactoeonline.databinding.ActivityGameBinding
 class GameActivity : AppCompatActivity(),OnClickListener{
 
     lateinit var binding: ActivityGameBinding
+
+    private var gameModel : GameModel? =null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityGameBinding.inflate(layoutInflater)
@@ -28,8 +31,46 @@ class GameActivity : AppCompatActivity(),OnClickListener{
         binding.startGameBtn.setOnClickListener {
             startGame();
         }
-    }
 
+        GameData.gameModel.observe(this){
+            gameModel = it
+            setUI()
+
+        }
+
+
+    }
+    fun setUI(){
+        gameModel?.apply {
+            binding.btn0.text=filledPos[0]
+            binding.btn1.text=filledPos[1]
+            binding.btn2.text=filledPos[2]
+            binding.btn3.text=filledPos[3]
+            binding.btn4.text=filledPos[4]
+            binding.btn5.text=filledPos[5]
+            binding.btn6.text=filledPos[6]
+            binding.btn7.text=filledPos[7]
+            binding.btn8.text=filledPos[8]
+
+            binding.gameStatusText.text=
+                when(gameStatus){
+                    GameStatus.CREATED->{
+                        "Game ID :"+gameId
+                    }
+                    GameStatus.JOINED->{
+                        "Click on start game"
+                    }
+                    GameStatus.INPROGRESS->{
+                        currentPlayer+"turn"
+                    }
+                    GameStatus.FINISHED->{
+                        if(winner.isNotEmpty()) winner+"Won"
+                        else "DRAW"
+                    }
+                }
+        }
+
+    }
     fun startGame() {
 
     }
